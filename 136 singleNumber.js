163a5b5f4dfefe20785ca13a9e15184b86216b8e
shawn <jpxx@umich.edu>
2016-10-17 22:49:15 -0400

    LeetCode in JavaScript

---
+++ 136 singleNumber.js
@@ -0,0 +1,15 @@
+// Given an array of integers, every element appears twice except for one. Find that single one.
+
+// Note:
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    var result = 0;
+    for (var i = 0, len = nums.length; i < len; i++) {
+      result ^= nums[i];
+    }
+    return result;
+};
